e684ef5ec35e23c6e81dd3fb1a7f385e024c62b2
MrSigma123 <karol.czolpinski@gmail.com>
2024-06-21 13:04:04 +0200

    Update to make colorscheme remain after plugin updates

--- nvim/lua/user/plugins/colorscheme.lua
+++ nvim/lua/user/plugins/colorscheme.lua
@@ -2,12 +2,17 @@ return {
   "folke/tokyonight.nvim",
   priority = 1000,
   config = function()
-    require("tokyonight").setup({
-      style = "night",
-      -- on_colors = function(colors)
-      -- end
-    })
+    local transparent = false -- set to true if you would like to enable transparency
 
+    require("tokyonight").setup({
+      style = "light",
+      transparent = transparent,
+      styles = {
+        sidebars = transparent and "transparent" or "light",
+        floats = transparent and "transparent" or "light",
+      },
+      on_colors = function() end,
+    })
     vim.cmd("colorscheme tokyonight")
   end,
 }
